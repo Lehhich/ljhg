171d9fe3b5ccd7c8f21c1c0c59736e86de036e05
lehhich <lukan445@gmail.com>
2024-10-27 11:15:47 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+<v-col class="12 cols" style="height: 50px; border: white solid">
+  <div class="d-flex" STYLE="font-size: 30px;margin-left: 5px;">JEPIX</div>
+
+</v-col>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
